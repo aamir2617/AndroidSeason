eb52d31ccb138805ab31bd1f3d7c312c2508004a
aamir2617 <aamirvahora1828@gmail.com>
2019-12-31 16:26:50 +0530

    merge solved by Amir

--- app/src/main/java/com/example/androidseason/MainActivity.java
+++ app/src/main/java/com/example/androidseason/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // commited by Amir
+        //555
         //77777777
     }
 }
