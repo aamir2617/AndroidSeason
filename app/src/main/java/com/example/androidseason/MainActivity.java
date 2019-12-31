07cb8b9d122494e40e26d9db67da2dc28ab8a31e
anish59 <anish.vahora5@gmail.com>
2019-12-31 16:24:35 +0530

    change by Anish

--- app/src/main/java/com/example/androidseason/MainActivity.java
+++ app/src/main/java/com/example/androidseason/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // commited by Amir
+        //77777777
     }
 }
